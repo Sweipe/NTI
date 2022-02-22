eed6292f6f386f710270636644f02a41fb51ad9f
Sweipe <56695050+Sweipe@users.noreply.github.com>
2022-02-22 17:30:14 +0100

    Add files via upload

---
+++ TryCatchFinally/TryCatchFinally/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs;
+            float ox, oy, tx, ty;
+            try {
+                Console.WriteLine("> This code will output a normalized vector by subtracting target from origin");
+                Console.WriteLine("> Please input two floats separeted by ';' per field, e.g. 2,25;6,75 or 2;5,0");
+                Console.Write("Origin point: ");
+                inputs = Console.ReadLine().Split(';');
+                ox = float.Parse(inputs[0]);
+                oy = float.Parse(inputs[1]);
+                Console.Write("Target point: ");
+                inputs = Console.ReadLine().Split(';');
+                tx = float.Parse(inputs[0]);
+                ty = float.Parse(inputs[1]);
+                Vector3 result = Direction(new Vector3(ox, oy), new Vector3(tx, ty));
+                Console.WriteLine($"Resulting normalized vector is: {result.ToString()}");
+            }
+            catch (Exception e) {
+                string type = e.GetType().Name.ToString();
+                if (type == "FormatException")
+                {
+                    Console.WriteLine("Your inputs are invalid");
+                }
+                else
+                {
+                    Console.WriteLine(type);
+                }
+            }
+            finally {
+                Console.WriteLine("\nCode has been executed without crashing.\nPress any key to exit");
+                Console.ReadKey();
+            }
+
+            Vector3 Direction(Vector3 origin, Vector3 target)
+            {
+                float x = target.x - origin.x;
+                float y = target.y - origin.y;
+                float unit = Convert.ToSingle(Math.Sqrt(x * x + y * y));
+                return new Vector3(x / unit, y / unit);
+            }
+        }
+    }
+
+    public class Vector3 {
+        public float x;
+        public float y;
+        public Vector3(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString() {
+            return $"{{{x}}}, {{{y}}}";
+        }
+    }
+}
